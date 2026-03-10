1a76a32e120d59d2847f692c2d5f3a8044fe9524
mrmattsheumack <mrmattsheumack@gmail.com>
2026-03-10 21:37:26 +1100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import PhotographyScout as App from './App.jsx'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
